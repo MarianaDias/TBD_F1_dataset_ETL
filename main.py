318dd766cf60d9773e0f6aa821d75c8be8f7d473
mapdias <mariana.dias@opus-software.com.br>
2021-09-30 20:18:22 -0300

    Loading dataset

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def load_csv_file(path):
+    try:
+        return pd.read_csv(path, encoding='ISO-8859-1')
+    except:
+        print("Não foi possível carregar o arquivo " + path)
+        return pd.DataFrame()
+
+def print_df(name, df):
+    print(name)
+    print(list(df))
+    print(df)
+
+def main():
+    # Load all dataset into dataframes
+    circuits_df = load_csv_file('f1_dataset/circuits.csv')
+    drivers_df = load_csv_file('f1_dataset/drivers.csv')
+    constructors_df = load_csv_file('f1_dataset/constructors.csv')
+    constructors_results_df = load_csv_file('f1_dataset/constructorResults.csv')
+    results_df = load_csv_file('f1_dataset/results.csv')
+    races_df = load_csv_file('f1_dataset/races.csv')
+    lap_time_df = load_csv_file('f1_dataset/lapTimes.csv')
+
+if __name__ == '__main__':
+    main()
