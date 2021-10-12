67f43b786279758af028024a8d66d0871eae5f5e
mapdias <mariana.dias@opus-software.com.br>
2021-10-12 19:26:43 -0300

    Init transformations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import transformation
 import mongodb_connection as mongo_conn
 import neo4j_connection as neo_conn
 
+
 def load_csv_file(path):
     try:
         return pd.read_csv(path, encoding='ISO-8859-1')
@@ -10,37 +11,53 @@ def load_csv_file(path):
         print("Não foi possível carregar o arquivo " + path)
         return pd.DataFrame()
 
+
 def print_df(name, df):
     print(name)
     print(list(df))
     print(df)
 
+
 def main():
     # Load all dataset into dataframes
-    # circuits_df = load_csv_file('f1_dataset/circuits.csv')
-    # drivers_df = load_csv_file('f1_dataset/drivers.csv')
-    # constructors_df = load_csv_file('f1_dataset/constructors.csv')
-    # constructors_results_df = load_csv_file('f1_dataset/constructorResults.csv')
-    # results_df = load_csv_file('f1_dataset/results.csv')
-    # races_df = load_csv_file('f1_dataset/races.csv')
-    # lap_time_df = load_csv_file('f1_dataset/lapTimes.csv')
-    #
-    # #Transformations
-    # tranformation.create_laptime_seconds(lap_time_df)
-    # tranformation.create_driver_fullname_col(drivers_df)
-    # tranformation.complete_drivers_code(drivers_df)
+    circuits_df = load_csv_file('f1_dataset/circuits.csv')
+    drivers_df = load_csv_file('f1_dataset/drivers.csv')
+    constructors_df = load_csv_file('f1_dataset/constructors.csv')
+    constructors_results_df = load_csv_file('f1_dataset/constructorResults.csv')
+    results_df = load_csv_file('f1_dataset/results.csv')
+    races_df = load_csv_file('f1_dataset/races.csv')
+    lap_time_df = load_csv_file('f1_dataset/lapTimes.csv')
 
-    #MongoDB Test Connection
-    # dbname = mconn.get_database(mconn.MONGO_CONNECTION_STR, mconn.COLLECTION_TEST)
+    # print_df("Circuitos", circuits_df)
+    # print_df("Pilotos", drivers_df)
+    # print_df("Equipes de Construtores", constructors_df)
+    # print_df("Resultado dos Construtores", constructors_results_df)
+    # print_df("Resultados", results_df)
+    # print_df("Corridas", races_df)
+    # print_df("Tempos de Volta", lap_time_df)
+
+    # Transformations
+    lap_time_df = transformation.create_laptime_seconds(lap_time_df)
+    drivers_df = transformation.create_driver_fullname_col(drivers_df)
+    drivers_df = transformation.complete_drivers_code(drivers_df)
+
+    transformation.create_championship_aggregate(races_df, constructors_df,constructors_results_df,drivers_df, results_df)
+
+    # print_df("", lap_time_df["seconds"])
+    # print_df("", drivers_df["fullname"])
+
+    # MongoDB Test Connection
+    # dbname = mongo_conn.get_database()
     # print(dbname)
     # mongo_conn.create_db_test_collection()
-    # mongo_conn.find_all(dbname[mconn.COLLECTION_TEST])
+    # mongo_conn.find_all(dbname[mongo_conn.COLLECTION_TEST])
 
-    #Neo4J test connection
+    # Neo4J test connection
     # neo_conn.create_driver_constructor_relation("Max Verstappen", "Redbull")
     # neo_conn.find_all()
     # neo_conn.close()
-    print("Descomente/Personalize os trechos que desejar executar")
+    # print("Descomente/Personalize os trechos que desejar executar")
+
 
 if __name__ == '__main__':
     main()
--- transformation.py
+++ transformation.py
@@ -1,17 +1,32 @@
 import pandas as pd
 
+
 def complete_drivers_code(drivers_df):
-    df = drivers_df.fillna(value = {'code': drivers_df['surname'].str[0:3] })
+    df = drivers_df.fillna(value={'code': drivers_df['surname'].str[0:3]})
     df['code'] = df['code'].str.upper()
-    print(df["code"])
     return df
+
 
 def create_driver_fullname_col(drivers_df):
     drivers_df["fullname"] = drivers_df["forename"] + " " + drivers_df["surname"]
-    print(drivers_df)
     return drivers_df
+
 
 def create_laptime_seconds(lap_time_df):
     lap_time_df['seconds'] = lap_time_df['milliseconds'] / 1000
-    print(lap_time_df['seconds'])
     return lap_time_df
+
+def create_championship_aggregate(races_df, constructors_df, constructors_results_df, drivers_df, results_df):
+    # print(list(races_df))
+    # print(list(constructors_df))
+    # print(list(constructors_results_df))
+    # print(list(drivers_df))
+    # print(list(results_df))
+
+    # Merged Constructor DF
+    merged_constructor_df = pd.merge(races_df, constructors_results_df, on='raceId')[["year", "constructorId", "points", "round"]]
+    merged_constructor_df = pd.merge(merged_constructor_df, constructors_df, on='constructorId')[["year", "constructorRef", "points", "round"]]
+
+    merged_driver_df = pd.merge(races_df, results_df, on='raceId')
+    print(list(merged_driver_df))
+
