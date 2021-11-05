ec3591127f91a0c4da28bad50bf0784c36aff8b0
mapdias <mariana.dias@opus-software.com.br>
2021-11-05 11:44:20 -0300

    Loading Neo4J data

---
+++ db_load.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import neo4j_connection as neo_conn
+
+def init_neo_load(drivers_df, constructor_df, races_df, results_df):
+    # neo_conn.create_neo_constrains()
+    # load_drivers_neo(drivers_df)
+    # load_constructors(constructor_df)
+    load_constructor_drivers_relation(constructor_df,drivers_df, races_df, results_df)
+
+
+def load_drivers_neo(drivers_df):
+    for id in drivers_df["driverId"].unique():
+        driver = drivers_df.loc[drivers_df["driverId"] == id][["fullname", "dob", "nationality", "code", "driverRef"]]
+        neo_conn.create_driver(driver.to_dict('records')[0])
+    print("Drivers Loaded to Neo")
+
+
+def load_constructors(constructors_df):
+    for id in constructors_df["constructorId"].unique():
+        constructor = constructors_df.loc[constructors_df["constructorId"] == id][
+            ["name", "constructorRef", "nationality"]]
+        neo_conn.create_constructor(constructor.to_dict('records')[0])
+    print("Constructors Loaded to Neo")
+
+
+def load_constructor_drivers_relation(constructor_df, driver_df, races_df, result_df):
+    relation = result_df[['raceId', 'driverId', 'constructorId']]
+    race_year = races_df[['raceId', 'year']]
+    relation = pd.merge(relation, race_year, on='raceId')[['driverId', 'constructorId', 'year']].drop_duplicates()
+    relation = pd.merge(relation, constructor_df, on='constructorId', how='left')[['driverId', 'constructorId',
+                                                                        'year', 'constructorRef']]
+    relation = pd.merge(relation, driver_df, on='driverId')[['year', 'constructorRef', 'driverRef']]
+    print(relation.loc[relation['driverRef'] == 'massa'])
+    # for i in range(0, relation['year'].count()):
+    #     neo_conn.create_driver_constructor_relation(relation.iloc[i])
+    # print("Relationships Created at Neo")
+
--- neo4j_connection.py
+++ neo4j_connection.py
@@ -1,23 +1,61 @@
-
 from neo4j import GraphDatabase
+from neo4j.exceptions import ConstraintError
 
 uri = "neo4j://localhost:7687"
 neo_driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
 
-def create_driver_constructor_relation(driver_name, constructor_name):
-    query = (
-            "CREATE (d:Driver { name: '" + driver_name + "' }) "
-            "CREATE (c:Constructor { name: '" + constructor_name + "' }) "
-            "CREATE (d)-[:DRIVEN_FOR]->(c) "
-    )
+
+def create_driver_constructor_relation_test(driver_name, constructor_name):
+    query = "CREATE (d:Driver { name: '%s' }) CREATE (c:Constructor { name: '%s' }) CREATE (d)-[:DRIVEN_FOR]->(c)" \
+            % (driver_name, constructor_name)
+
     with neo_driver.session() as session:
         session.run(query)
+
+
+def create_driver_constructor_relation(driver_constructor):
+    query = "MATCH (d:Driver {ref:'%s'}),(c:Constructor {ref:'%s'}) CREATE ((d)-[r:DRIVEN_FOR {year: %d}]->(c))" % \
+            (driver_constructor["driverRef"], driver_constructor["constructorRef"], driver_constructor["year"])
+    with neo_driver.session() as session:
+        session.run(query)
+
 
 def find_all():
     query = "MATCH p=()-[r:DRIVEN_FOR]->() RETURN p"
     with neo_driver.session() as session:
         result = session.run(query)
         print(result.values())
+
+
+def create_constructor(constructor):
+    query = "CREATE(d: Constructor { name: '%s' , ref: '%s', nationality: '%s' })" % (constructor["name"],
+                                                                                      constructor["constructorRef"],
+                                                                                      constructor["nationality"])
+    with neo_driver.session() as session:
+        try:
+            session.run(query)
+        except ConstraintError:
+            print("Constructor already exists {0}".format(constructor["constructorRef"]))
+
+
+def create_driver(driver):
+    query = "CREATE(d: Driver { name: '%s' ,code: '%s', " \
+            "ref: '%s', dob: '%s', nationality: '%s' })" % (driver["fullname"], driver["code"], driver["driverRef"],
+                                                            driver["dob"], driver["nationality"])
+    with neo_driver.session() as session:
+        try:
+            session.run(query)
+        except ConstraintError:
+            print("Driver already exists {0}".format(driver["driverRef"]))
+
+
+def create_neo_constrains():
+    constrain_constructor = "CREATE CONSTRAINT ON (c:Constructor) ASSERT c.ref IS UNIQUE"
+    constrain_driver = "CREATE CONSTRAINT ON (c:Driver) ASSERT c.ref IS UNIQUE"
+    with neo_driver.session() as session:
+        session.run(constrain_constructor)
+        session.run(constrain_driver)
+
 
 def close():
     neo_driver.close()
