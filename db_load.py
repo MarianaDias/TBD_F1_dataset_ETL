439ded86d194774b976012b153cfeab4b6f80705
mapdias <mariana.dias@opus-software.com.br>
2021-11-10 10:21:53 -0300

    Loanding neo data complete

--- db_load.py
+++ db_load.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import neo4j_connection as neo_conn
 
+
 def init_neo_load(drivers_df, constructor_df, races_df, results_df):
-    # neo_conn.create_neo_constrains()
-    # load_drivers_neo(drivers_df)
-    # load_constructors(constructor_df)
-    load_constructor_drivers_relation(constructor_df,drivers_df, races_df, results_df)
+    neo_conn.create_neo_constrains()
+    load_drivers_neo(drivers_df)
+    load_constructors(constructor_df)
+    load_constructor_drivers_relation(constructor_df, drivers_df, races_df, results_df)
 
 
 def load_drivers_neo(drivers_df):
@@ -28,10 +29,8 @@ def load_constructor_drivers_relation(constructor_df, driver_df, races_df, resul
     race_year = races_df[['raceId', 'year']]
     relation = pd.merge(relation, race_year, on='raceId')[['driverId', 'constructorId', 'year']].drop_duplicates()
     relation = pd.merge(relation, constructor_df, on='constructorId', how='left')[['driverId', 'constructorId',
-                                                                        'year', 'constructorRef']]
+                                                                                   'year', 'constructorRef']]
     relation = pd.merge(relation, driver_df, on='driverId')[['year', 'constructorRef', 'driverRef']]
-    print(relation.loc[relation['driverRef'] == 'massa'])
-    # for i in range(0, relation['year'].count()):
-    #     neo_conn.create_driver_constructor_relation(relation.iloc[i])
-    # print("Relationships Created at Neo")
-
+    for i in range(0, relation['year'].count()):
+        neo_conn.create_driver_constructor_relation(relation.iloc[i])
+    print("Relationships Created at Neo")
